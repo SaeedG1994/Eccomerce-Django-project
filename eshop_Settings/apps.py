942d6ff64bed5aa3483f6bfa7af60b6a34545758
Bahman-dev <Bahman.gs2020@gmail.com>
2022-06-10 18:14:45 +0430

    FC- 9 -- Add Eshop_Settings

---
+++ eshop_Settings/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class EshopUserConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'eshop_User'
+    verbose_name = ' کاربران سایت '
